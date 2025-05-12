2441dc76a925d1652486586abc6427d76e252fb1
Cacao <cacao.tech59@gmail.com>
2025-05-12 14:57:49 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+import discord
 
+token = "MTM3MTQ2OTYzNDYxNTc3MTE5Ng.GuwHVm.Wzz6_YVthwmYiTvqS4yZxXICu6K0ksy7u-kik8"
+
+client = discord.Client(intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+  print("ready")
+
+client.run(token=token)
